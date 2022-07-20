8f8dfb63d69bee23f98fcf6b1ced1c5cb44c81a3
ShivaniUpa01 <108825975+ShivaniUpa01@users.noreply.github.com>
2022-07-20 20:35:45 +0530

    Update Remove-Duplicates.py

--- Remove-Duplicates.py
+++ Remove-Duplicates.py
@@ -1,8 +1,5 @@
 def remove_duplicates(test):
-  print(test)
   check = {}
-  print(check)
   for i in test:
-    print(check)
     check[i] = check[i]+1 if check.get(i) else 1
   return(check.keys())
