62b6c04c44013e7423918f17d3e1bb0ab894bedf
shivani <shivani.01upadhyay@gmail.com>
2022-09-20 14:59:08 +0530

    Number add into valriable on user demand

---
+++ P11-Count-numbers.py
@@ -0,0 +1,15 @@
+# Write a program to enter the numbers till the user wants and at the end it should display the count of positive,
+# negative and zeros entered.
+def count_all_number():
+    i = "1"
+    count = 0
+    while i == "1":
+        num = int(input("Number : "))
+        count = 10 * count + num
+        print(count)
+        i = "0"
+        i = input("Continue if yes press 1 for no press 0 : ")
+
+
+if __name__ == "__main__":
+    count_all_number()
